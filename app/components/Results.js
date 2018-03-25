ae890fa09fe625e60cd3798555376dc083b9ece2
Peter Tountas <petertountas1@gmail.com>
2018-03-25 17:18:03 -0500

    updates to api

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,8 +1,20 @@
 var React = require('react');
+var queryString = require('query-string');
+var api = require('../utils/api');
 
 class Results extends React.Component{
+  componentDidMount() {
+    var players = queryString.parse(this.props.location.search)
+    api.battle([
+      players.playerOneName,
+      players.playTwoname
+    ]).then(function(results){
+      console.log(results)
+    })
+  }
+
   render() {
-    console.log(this.props)
+  
     return (
       <div>Results</div>
     )
