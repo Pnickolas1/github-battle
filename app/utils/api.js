ea5933251fac44587aaf8b9190bf8fdbfc30ca73
Peter Tountas <petertountas1@gmail.com>
2018-03-25 16:37:45 -0500

    more updates

--- app/utils/api.js
+++ app/utils/api.js
@@ -16,16 +16,48 @@ function getRepos(username){
   return axios.get(`https://api.github.com/users/${username}/repos/${params}&per_page=100`)
 }
 
-function getStartCount(repos){
+function getStarCount(repos){
   return repos.data.reduce(function (counter, item){
     return counter + item.stargazers_count
   }, 0)
 }
 
+function calculateScore(profile, repos){
+  var followers = profile.followers;
+  var totalStars = getStarCount(repos)
+  return ( followers * 3 ) + totalStars
+}
+
+function handleError(error){
+  console.warn(error)
+  return null;
+}
+
+function getUserData(player){
+  return axios.all([
+    getProfile(player),
+    getRepos(player)
+  ]).then(function(data){
+    var profile = data[0];
+    var repos = data[1];
+    return {
+      profile: profile,
+      score: calculateScore(profile, repos)
+    }
+  })
+}
+
+function sortPlayers(players){
+  return players.sort(function(a,b){
+    return b.score - a.score
+  });
+}
 
 module.exports = {
   battle: function (players) {
-
+    return axios.all(players.map(getUserData))
+      .then(sortPlayers)
+      .catch(handleError)
   },
   fetchPopularRepos: function(language ){
     var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
