806a9423c204fa899ba7291d84e69eeb1b68155a
Peter Tountas <petertountas1@gmail.com>
2018-04-07 18:21:25 -0500

    more updates

--- app/components/PlayerPreview.js
+++ app/components/PlayerPreview.js
@@ -1,7 +1,7 @@
 const React = require('react');
 const PropTypes = require('prop-types');
 
-function PlayerPreview({ avatar, username }){
+function PlayerPreview({ avatar, username, children }){
   return (
     <div>
       <div className='column'>
